772a4f86a2de270f9178631880703f57404da6b0
kida21 <kidusp22@gmail.com>
2022-09-29 13:27:41 +0300

    Database connection
    Database connection

--- project 2/project 2/model/model.cs
+++ project 2/project 2/model/model.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
+using System.Data.SqlClient;
 
 namespace project_2.model
 {
@@ -27,11 +28,55 @@ namespace project_2.model
         
         public void save()
         {
-           modList.Add(this);
-           MessageBox.Show(" added succesfully");
+            try
+            {
+                SqlConnection con = new SqlConnection("data source = .;inital catalog=prjectdb;integrated security=true;");
+                con.Open();
+                int y;
+                if (this.isavailable == true)
+                {
+                    y=0;
+                }
+                else
+                {
+                    y = 1;
+                }
+                SqlCommand cmd = new SqlCommand($" insert into item values({this.number},'{this.date}',{this.inventorynumber},'{this.objectname}',{this.price},'{this.count}',{y})", con);
+                cmd.ExecuteReader();
+
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+           // modList.Add(this);
+           //MessageBox.Show(" added succesfully");
         }
         public  static List<mod> getallproduct() { 
-        
+           List<mod> modlist = new List<mod>();
+            try
+            {
+                SqlConnection con = new SqlConnection("data source = .;inital catalog=prjectdb;integrated security=true;");
+                con.Open();
+                SqlCommand cmd = new SqlCommand($" select * from mod ", con);
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    modlist.Add(new mod()
+                    {
+                        number = dr["number"].ToString(),
+                        date = dr["date"].ToString(),
+                        inventorynumber = dr["inventorynum"].ToString(),
+                        objectname = dr["objname"].ToString(),
+                        price = dr["price"].ToString(),
+                        count = dr["count"].ToString(),
+                        isavailable = (bool)dr["isavail"]
+                    }
+                    );
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
             return modList;
         
          }
